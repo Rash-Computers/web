7736a16771e3039cf69391967a9f9b314cc94d18
Brice Duke <bricewduke@gmail.com>
2021-10-12 21:44:10 -0500

    feat(partners): add partners component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from 'react';
 
 import Hero from '../components/home/Hero';
+import Partners from '../components/home/Partners';
 import Programs from '../components/home/Programs';
 
 export default function Home(): ReactElement {
@@ -8,6 +9,7 @@ export default function Home(): ReactElement {
 		<div className="h-screen mx-28">
 			<Hero />
 			<Programs />
+			<Partners />
 		</div>
 	);
 }
