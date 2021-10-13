8ef0fae85eee1dbb9cbc851d5543e2b2ba0bcfd3
Brice Duke <bricewduke@gmail.com>
2021-10-12 20:08:24 -0500

    feat(global): add tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,5 +3,5 @@ import React, { ReactElement } from 'react';
 interface Props {}
 
 export default function Home({}: Props): ReactElement {
-	return <div>hi</div>;
+	return <div className="text-xl">hi</div>;
 }
