1c9ae46d31dbae61af490fdf3d1f6f43132e4ad5
Brice Duke <bricewduke@gmail.com>
2021-10-17 14:53:40 -0500

    feat(global): set up modal

---
+++ components/global/Layout/index.tsx
@@ -0,0 +1,16 @@
+import React, { ReactElement } from 'react';
+
+import NavBar from '../NavBar';
+
+interface Props {
+	children: React.ReactNode;
+}
+
+export default function Layout({ children }: Props): ReactElement {
+	return (
+		<div>
+			<NavBar />
+			<div className="h-screen mx-28">{children}</div>
+		</div>
+	);
+}
--- components/global/NavBar/index.tsx
+++ components/global/NavBar/index.tsx
@@ -1,18 +1,37 @@
-import React, { ReactElement } from 'react';
+import Link from 'next/link';
+import React, { ReactElement, useState } from 'react';
+
+import AuthModal from '../../modal/AuthModal';
 
 export default function NavBar(): ReactElement {
+	const [modal, setModal] = useState(false);
+
+	const openModal = () => setModal(true);
+	const closeModal = () => setModal(false);
+
 	return (
 		<nav className="sticky top-0 h-16 w-screen bg-white px-28 flex justify-between items-end py-1 text-textSecondary font-bold text-xl">
 			<div>
-				<h1>
-					Rash <span className="text-primary">Computers</span>
-				</h1>
+				<Link href="/">
+					<a>
+						Rash <span className="text-primary">Computers</span>
+					</a>
+				</Link>
 			</div>
 			<div className="space-x-9">
-				<button className="font-bold">Programs</button>
-				<button className="font-bold">Create Account</button>
+				<Link href="/programs">
+					<a className="font-bold">Programs</a>
+				</Link>
+				<button
+					onClick={() => (modal ? closeModal() : openModal())}
+					className="font-bold"
+				>
+					Create Account
+				</button>
 				<button className="font-bold">Login</button>
 			</div>
+
+			{modal && <AuthModal handleClose={closeModal} />}
 		</nav>
 	);
 }
