a6815083ee7d0bbf12b4bd0e9c38b8612ff20446
ntwari egide <ntwariegide2@gmail.com>
2021-11-14 13:33:27 -0800

    fix(font): add all poppins font weights

--- components/home/Hero/index.tsx
+++ components/home/Hero/index.tsx
@@ -2,21 +2,21 @@ import React, { ReactElement } from 'react';
 
 export default function Hero(): ReactElement {
 	return (
-		<div className="flex justify-between items-center h-full space-x-56">
-			<div className="flex flex-col h-1/2 justify-between items-start">
+		<div className="flex justify-between items-center h-full">
+			<div className="flex w-2/3 flex-col h-1/2 justify-between items-start">
 				<article>
-					<h1 className="text-6xl font-bold mb-3 leading-tight">
+					<h1 className="text-3xl font-medium">
 						Learn a new skill and prepare yourself for the Fourth Industrial
 						Revolution
 					</h1>
 
-					<h2 className="text-3xl">
+					<h2 className="text-xl mt-8 font-light">
 						Our Programs ranges from Web development, to Artificial Intelligence
 						and Machine Learning
 					</h2>
 				</article>
 
-				<button className="bg-primary text-white font-semibold text-3xl px-5 py-3 rounded-full">
+				<button className="bg-primary text-white text-md px-14 py-4 rounded-md hover:border-indigo-600 hover:transition">
 					Get Started
 				</button>
 			</div>
@@ -24,7 +24,7 @@ export default function Hero(): ReactElement {
 			<img
 				src="/assets/home/hero.svg"
 				alt="Computer Artwork"
-				className="-mt-20"
+				className="-mt-20 ml-8 w-full left-8 welcome-bg-photo"
 			/>
 		</div>
 	);
