8e4d423866918f0f7d31f8aaa22081e717d7cb62
ntwari-egide <ntwariegide2@gmail.com>
2021-11-15 04:40:04 -0800

    ft(screen): implement new screen

--- components/modal/AuthModal/ResetScreen.tsx
+++ components/modal/AuthModal/ResetScreen.tsx
@@ -1,4 +1,8 @@
-import React, { ReactElement } from 'react';
+import { Divider, Modal , Typography} from 'antd';
+import React, { ReactElement, useState } from 'react';
+
+const {Title, Text} = Typography
+
 
 type ScreenProps = 'login' | 'register' | 'reset';
 
@@ -7,37 +11,48 @@ interface Props {
 }
 
 export default function ResetScreen({ changeScreen }: Props): ReactElement {
+
+	const [resetmodalvisibility,setresetmodalvisibility] = useState(true)
+
+
+
 	return (
-		<div className="flex flex-col justify-evenly h-full items-center w-4/6">
-			<div className="w-full text-center space-y-4">
-				<p className="text-xl font-medium">Reset Your Password</p>
-				<form
-					action=""
-					className="flex flex-col justify-evenly items-center space-y-8 w-full"
-				>
-					<input
-						type="email"
-						name="email"
-						placeholder="Your email address"
-						className="p-4 border-textSecondary border rounded-2xl w-full"
-					/>
-					<input
-						type="submit"
-						value="Reset Password"
-						className="cursor-pointer w-full py-3 text-2xl font-semibold bg-purple-400 px-4 rounded-2xl border border-textSecondary mb-4"
-					/>
-				</form>
+		<Modal title={
+			<Title level={3} className="text-center text-gray-700 text-xl">Reset Your Password</Title>
+		} footer={null} visible={resetmodalvisibility}  onCancel={()=> setresetmodalvisibility(false)}>
+			<div className="flex flex-col justify-between items-center h-full">
+				<div className="w-full text-center space-y-4">
+					<form
+						action=""
+						className="flex flex-col justify-evenly items-center space-y-8 w-full"
+					>
+						<input
+							type="email"
+							name="email"
+							placeholder="Your email address"
+							className="p-4 border rounded-md w-96 outline-none"
+						/>
+						<input
+							type="submit"
+							value="Reset Password"
+							className="cursor-pointer bg-primary text-white text-md px-14 py-4 rounded-md hover:border-indigo-600 hover:transition w-96"
+						/>
+					</form>
+				</div>
+				<div className="w-full text-center space-y-4">
+
+					<Divider className="w-2/3">
+						<Text className="text-gray-500 text-sm">Or Reauthenticate</Text>
+					</Divider>
+					<button className="cursor-pointer bg-primary text-white text-md px-14 py-4 rounded-md hover:border-indigo-600 hover:transition w-96">
+						Login with GitHub
+					</button>
+				</div>
+				<p className="text-sm text-center mb-8 w-96 mt-8 font-normal">
+					We will send you a link to reset your password. If you signed up via
+					github you need just need to log in again.
+				</p>
 			</div>
-			<div className="w-full text-center space-y-4">
-				<p className="text-xl font-medium">Or Reauthenticate</p>
-				<button className="w-full p-4 text-2xl font-semibold bg-purple-400 rounded-2xl border border-textSecondary mb-6">
-					Login with GitHub
-				</button>
-			</div>
-			<p className="text-xl font-medium">
-				We will send you a link to reset your password. If you signed up via
-				github you need just need to log in again.
-			</p>
-		</div>
+		</Modal>
 	);
 }
--- components/programs/SchoolCard/index.tsx
+++ components/programs/SchoolCard/index.tsx
@@ -13,13 +13,13 @@ export default function SchoolCard({
 }: Props): ReactElement {
 	return (
 		<div className="flex justify-between items-start mb-56">
-			<article className="">
-				<header className="text-primary text-4xl font-bold mb-10">
+			<article className="mt-20">
+				<header className="text-primary text-2xl font-semibold mb-10">
 					{name}
 				</header>
-				<p className="leading-60pt text-4xl w-5/6">{description}</p>
+				<p className="text-base leading-8 w-5/6">{description}</p>
 			</article>
-			<img src={`/assets/programs/${imageUrl}.svg`} alt={name} />
+			<img src={`/assets/programs/${imageUrl}.svg`} className="w-2/5" alt={name} />
 		</div>
 	);
 }
