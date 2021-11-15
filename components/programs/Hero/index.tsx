56195365d2cd5c71810043d4ee6de7a659f2ad6c
ntwari-egide <ntwariegide2@gmail.com>
2021-11-15 00:44:57 -0800

    ft(programe-welcome): update styles

--- components/programs/Hero/index.tsx
+++ components/programs/Hero/index.tsx
@@ -4,18 +4,18 @@ export default function ProgramsHero(): ReactElement {
 	return (
 		<div className="flex justify-between items-center h-full">
 			<div className="flex items-start justify-between">
-				<article>
-					<h1 className="text-6xl font-bold mb-8 leading-tight">
+				<article className="w-2/5">
+					<h1 className="text-3xl font-semibold mb-8 leading-tight">
 						Upgrade your skills by Enrolling in one of our Schools.
 					</h1>
 
-					<h2 className="text-3xl leading-loose">
+					<h2 className="text-xl font-normal leading-loose">
 						The Curriculums are categorized in schools. Chose your relevant
 						paths by enrolling in one of the schools
 					</h2>
 				</article>
 
-				<img src="/assets/programs/hero.svg" alt="Code" />
+				<img src="/assets/programs/hero.svg" className="w-2/5" alt="Code" />
 			</div>
 		</div>
 	);
