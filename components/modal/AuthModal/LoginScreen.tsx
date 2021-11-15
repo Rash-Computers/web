5688df61bdafe8e0a94b4fd53a271c2b270b3938
ntwari-egide <ntwariegide2@gmail.com>
2021-11-14 23:48:55 -0800

    ft(login-modal): improve login modal design

--- components/modal/AuthModal/LoginScreen.tsx
+++ components/modal/AuthModal/LoginScreen.tsx
@@ -1,4 +1,7 @@
-import React, { ReactElement } from 'react';
+import { Divider, Modal, Typography } from 'antd';
+import React, { ReactElement, useState } from 'react';
+
+const {Title,Text} = Typography
 
 type ScreenProps = 'login' | 'register' | 'reset';
 
@@ -7,56 +10,63 @@ interface Props {
 }
 
 export default function RegisterScreen({ changeScreen }: Props): ReactElement {
+ 
+
+	const [loginmodalvisibility,setloginmodalvisibility] = useState(true)
+
+
 	return (
-		<div className="flex flex-col justify-evenly h-full w-4/6">
-			<div>
-				<button className="w-full py-3 text-4xl font-semibold bg-purple-400 px-4 rounded-2xl border border-textSecondary mb-6">
-					Login with GitHub
-				</button>
-				<div className="w-full flex items-center justify-center space-x-4">
-					<div className="bg-textSecondary w-1/6 h-[2px]"></div>
-					<span>or login with email</span>
-					<div className="bg-textSecondary w-1/6 h-[2px]"></div>
+		<Modal title={
+			<Title level={3} className="text-center text-gray-700">Create an account</Title>
+		} footer={null} visible={loginmodalvisibility} onCancel={()=> setloginmodalvisibility(false)}>
+			<div className="flex flex-col justify-between items-center h-full">
+				<div>
+					<button className="bg-primary text-white text-md px-14 py-4 rounded-md hover:border-indigo-600 hover:transition w-96">
+						Login with GitHub
+					</button>
+					<Divider className="w-96">
+						<Text className="text-gray-500 text-sm">Or login with Google</Text>
+					</Divider>
+				</div>
+				<form className="flex flex-col justify-evenly items-center space-y-6">
+					<input
+						type="email"
+						name="email"
+						placeholder="Email"
+						className="p-4 border rounded-md w-96 outline-none"
+					/>
+					<input
+						type="password"
+						name="password"
+						placeholder="Password"
+						className="p-4 border rounded-md w-96 outline-none"
+					/>
+					<input
+						type="submit"
+						value="Log in"
+						className="cursor-pointer bg-primary text-white text-md px-14 py-4 rounded-md hover:border-indigo-600 hover:transition w-96"
+					/>
+				</form>
+				<div className="text-sm font-normal flex justify-between mt-14 mb-16">
+					<span>
+						Need an account?
+						<a
+							className="ml-1 hover:text-primary cursor-pointer"
+							onClick={() => changeScreen('register')}
+						>
+							Register
+						</a>
+					</span>
+					<span>
+						<a
+							className="text-primary cursor-pointer ml-8"
+							onClick={() => changeScreen('reset')}
+						>
+							Forgot Password?
+						</a>
+					</span>
 				</div>
 			</div>
-			<form className="flex flex-col justify-evenly items-center space-y-6">
-				<input
-					type="email"
-					name="email"
-					placeholder="Email"
-					className="p-4 border-textSecondary border rounded-2xl w-full"
-				/>
-				<input
-					type="password"
-					name="password"
-					placeholder="Password"
-					className="p-4 border-textSecondary border rounded-2xl w-full"
-				/>
-				<input
-					type="submit"
-					value="Sign Up"
-					className="cursor-pointer w-full py-3 text-4xl font-semibold bg-purple-400 px-4 rounded-2xl border border-textSecondary mb-4"
-				/>
-			</form>
-			<div className="text-sm font-normal flex justify-between">
-				<span>
-					Need an account?
-					<a
-						className="ml-3 hover:text-primary cursor-pointer"
-						onClick={() => changeScreen('register')}
-					>
-						Register
-					</a>
-				</span>
-				<span>
-					<a
-						className="text-primary cursor-pointer"
-						onClick={() => changeScreen('reset')}
-					>
-						Forgot Password?
-					</a>
-				</span>
-			</div>
-		</div>
+		</Modal>
 	);
 }
