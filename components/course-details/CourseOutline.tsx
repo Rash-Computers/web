0e6fae90d86c092632cff62eb02446b4f18af106
ntwari-egide <ntwariegide2@gmail.com>
2021-11-27 00:36:52 -0800

    ft(course-details): implement course routing

--- components/course-details/CourseOutline.tsx
+++ components/course-details/CourseOutline.tsx
@@ -3,7 +3,7 @@
  * @description: course outline component
  */
 
-
+ import Link from 'next/link';
  import { ReactElement } from "react"
  import {Space, Typography, Divider} from "antd"
 
@@ -20,38 +20,48 @@ const {Title} = Typography
                     <Space>LEVEL</Space>
                 </div>
                 <hr className="border-gray-500 mt-4 w-[70vw]" />
-                <div className="flex cursor-pointer -ml-2 pl-2 hover:bg-gray-400 py-2 w-[70.6vw] transition-all flex-row text-base mt-8 text-gray-800">
-                    <div className=" w-[50vw] flex flex-row mt-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zM10.622 8.415a.4.4 0 0 0-.622.332v6.506a.4.4 0 0 0 .622.332l4.879-3.252a.4.4 0 0 0 0-.666l-4.88-3.252z" fill="rgba(64,64,64,1)"/></svg>
-                        <p className="ml-4">Introduction to machine learning</p>
+                
+                <Link href="/course-video">
+                    <div className="flex cursor-pointer -ml-2 pl-2 hover:bg-gray-400 py-2 w-[70.6vw] transition-all flex-row text-base mt-8 text-gray-800">
+                        <div className=" w-[50vw] flex flex-row mt-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zM10.622 8.415a.4.4 0 0 0-.622.332v6.506a.4.4 0 0 0 .622.332l4.879-3.252a.4.4 0 0 0 0-.666l-4.88-3.252z" fill="rgba(64,64,64,1)"/></svg>
+                            <p className="ml-4">Introduction to machine learning</p>
+                        </div>
+                        <Space>Beginner</Space>
                     </div>
-                    <Space>Beginner</Space>
-                </div>
+                </Link>
 
-                <div className="flex cursor-pointer -ml-2 pl-2 hover:bg-gray-400 py-2 w-[70.6vw] transition-all flex-row text-base mt-4 text-gray-800">
-                    <div className=" w-[50vw] flex flex-row mt-4" >
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zM10.622 8.415a.4.4 0 0 0-.622.332v6.506a.4.4 0 0 0 .622.332l4.879-3.252a.4.4 0 0 0 0-.666l-4.88-3.252z" fill="rgba(64,64,64,1)"/></svg>
-                        <p className="ml-4">Linear Regression</p>
+                <Link href="/course-video">
+                    <div className="flex cursor-pointer -ml-2 pl-2 hover:bg-gray-400 py-2 w-[70.6vw] transition-all flex-row text-base mt-4 text-gray-800">
+                        <div className=" w-[50vw] flex flex-row mt-4" >
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zM10.622 8.415a.4.4 0 0 0-.622.332v6.506a.4.4 0 0 0 .622.332l4.879-3.252a.4.4 0 0 0 0-.666l-4.88-3.252z" fill="rgba(64,64,64,1)"/></svg>
+                            <p className="ml-4">Linear Regression</p>
+                        </div>
+                        <Space>Intermediate</Space>
                     </div>
-                    <Space>Intermediate</Space>
-                </div>
 
-                <div className="flex cursor-pointer -ml-2 pl-2 hover:bg-gray-400 py-2 w-[70.6vw] transition-all flex-row text-base mt-4 text-gray-800">
-                    <div className=" w-[50vw] flex flex-row mt-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zM10.622 8.415a.4.4 0 0 0-.622.332v6.506a.4.4 0 0 0 .622.332l4.879-3.252a.4.4 0 0 0 0-.666l-4.88-3.252z" fill="rgba(64,64,64,1)"/></svg>
-                        <p className="ml-4">Classification</p>
+                </Link>
+
+                <Link href="/course-video">
+                    <div className="flex cursor-pointer -ml-2 pl-2 hover:bg-gray-400 py-2 w-[70.6vw] transition-all flex-row text-base mt-4 text-gray-800">
+                        <div className=" w-[50vw] flex flex-row mt-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zM10.622 8.415a.4.4 0 0 0-.622.332v6.506a.4.4 0 0 0 .622.332l4.879-3.252a.4.4 0 0 0 0-.666l-4.88-3.252z" fill="rgba(64,64,64,1)"/></svg>
+                            <p className="ml-4">Classification</p>
+                        </div>
+                        <Space>Intermediate</Space>
                     </div>
-                    <Space>Intermediate</Space>
-                </div>
+                </Link>
 
-                <div className="flex cursor-pointer -ml-2 pl-2 hover:bg-gray-400 py-2 w-[70.6vw] transition-all flex-row text-base mt-4 text-gray-800">
-                    <div className=" w-[50vw] flex flex-row mt-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zM10.622 8.415a.4.4 0 0 0-.622.332v6.506a.4.4 0 0 0 .622.332l4.879-3.252a.4.4 0 0 0 0-.666l-4.88-3.252z" fill="rgba(64,64,64,1)"/></svg>
-                        <p className="ml-4">Machine Learning projects</p>
+                
+                <Link href="/course-video">
+                    <div className="flex cursor-pointer -ml-2 pl-2 hover:bg-gray-400 py-2 w-[70.6vw] transition-all flex-row text-base mt-4 text-gray-800">
+                        <div className=" w-[50vw] flex flex-row mt-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zM10.622 8.415a.4.4 0 0 0-.622.332v6.506a.4.4 0 0 0 .622.332l4.879-3.252a.4.4 0 0 0 0-.666l-4.88-3.252z" fill="rgba(64,64,64,1)"/></svg>
+                            <p className="ml-4">Machine Learning projects</p>
+                        </div>
+                        <Space>Beginner</Space>
                     </div>
-                    <Space>Beginner</Space>
-                </div>
-
+                </Link>
                
                 <div className="flex cursor-pointer -ml-2 pl-2 py-2 w-[70.6vw] transition-all flex-row text-base mt-8 text-gray-800">
                     <div className=" w-[50vw] flex flex-row mt-4">
--- components/programs/SchoolCard/index.tsx
+++ components/programs/SchoolCard/index.tsx
@@ -1,4 +1,5 @@
 import React, { ReactElement } from 'react';
+import Link from 'next/link';
 
 interface Props {
 	name: string;
@@ -12,14 +13,16 @@ export default function SchoolCard({
 	imageUrl,
 }: Props): ReactElement {
 	return (
-		<div className="flex justify-between items-start mt-56">
-			<article className="mt-20">
-				<header className="text-primary text-3xl font-semibold mb-10">
-					{name}
-				</header>
-				<p className="text-2xl leading-8 w-5/6">{description}</p>
-			</article>
-			<img src={`/assets/programs/${imageUrl}.svg`} className="w-2/5" alt={name} />
-		</div>
+		<Link href="/course-details">
+			<div className="flex justify-between items-start mt-56 cursor-pointer">
+				<article className="mt-20">
+					<header className="text-primary text-3xl font-semibold mb-10">
+						{name}
+					</header>
+					<p className="text-2xl leading-8 w-5/6">{description}</p>
+				</article>
+				<img src={`/assets/programs/${imageUrl}.svg`} className="w-2/5" alt={name} />
+			</div>
+		</Link>
 	);
 }
