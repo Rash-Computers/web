ecefe00a95a8097bc1b31f19c6a432b8f27b91c6
ntwari-egide <ntwariegide2@gmail.com>
2021-11-30 12:01:45 -0800

    ft(course-video): implement dynamic page

--- pages/course-video.tsx
+++ pages/course-video.tsx
@@ -4,7 +4,6 @@
  */
 
 import * as actions from '../redux/actions/programs-actions'
-import { Button } from "antd";
 import { ReactElement, useEffect } from "react";
 import Layout from "../components/global/Layout";
 import { useAppSelector } from '../redux/store';
@@ -13,12 +12,14 @@ import { useAppSelector } from '../redux/store';
 export default function CourseVideoComponent(): ReactElement {
 
     const currentCourseOutline = useAppSelector(state => state?.currentSchoolOutlineDetails)
-
-    console.log('data: ',currentCourseOutline);    
+    
+    const lessonVideoDetails = useAppSelector( state => state?.lessonVideo)
 
     useEffect(() => {
         actions.getCurrentCourseOutlineDetails()
+        // actions.getLessonVideoDetails()
     },[currentCourseOutline])
+    
 
 	return (
 		<Layout>
@@ -32,8 +33,10 @@ export default function CourseVideoComponent(): ReactElement {
                 <div className="-mr-28 pl-28 pt-4 border-t-[1px] border-gray-300 w-[89vw] h-[90vh]">
 
                     <h1 className="text-center text-xl -ml-16">1. {currentCourseOutline?.outline}</h1>
-                    <iframe className="mt-8 w-[67.5vw] h-[65vh]" src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1&mute=1">
-                    </iframe>
+                    {/* <iframe className="mt-8 w-[67.5vw] h-[65vh]" src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1&mute=1">
+                    </iframe> */}
+
+                    <iframe className="mt-8 w-[67.5vw] h-[65vh]" width="560" height="315" src="https://www.youtube.com/embed/eIrMbAQSU34" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
 
                     <button className="hover:bg-indigo-500 transition-all hover:border-none hover:text-white bg-transparent border-gray-700 border-[1px] px-9 text-center rounded-3xl mt-8 py-2 font-semibold float-right mr-24">Next</button>
 
